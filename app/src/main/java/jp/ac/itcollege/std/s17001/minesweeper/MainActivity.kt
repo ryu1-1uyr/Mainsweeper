88f0423075279ce8056c277a8f8f9fa7312c8389
s17001 <s17001@std.it-college.ac.jp>
2018-12-12 11:57:52 +0900

    commit

---
+++ app/src/main/java/jp/ac/itcollege/std/s17001/minesweeper/MainActivity.kt
@@ -0,0 +1,15 @@
+package jp.ac.itcollege.std.s17001.minesweeper
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // View要素を変数に代入
+
+    }
+}
